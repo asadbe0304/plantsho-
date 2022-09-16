1cd8fe4aca5aec94ae313c8ee5575bc7f9441146
asadbe0304 <asadbekaxmadkulov@gmail.com>
2022-09-16 19:34:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "0px";
+    }
+}
